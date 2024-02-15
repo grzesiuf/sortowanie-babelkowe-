bbc53be39be5d6ceb25538155632e8f3ef0f2b87
krzysxpp22 <155613723+krzysxpp22@users.noreply.github.com>
2024-02-15 09:32:59 +0100

    da

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include "QMessageBox"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -16,7 +16,16 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
+    QString elementySTR = "";
+    QString el;
+    int n = elementy.length();
     elementy.append(ui->spinBox->value());
+    ui->spinBox->clear();
+    for(int i = 0;i<n;i++){
+        el = QString::number(elementy[i]);
+        elementySTR += el + " ";
+    }
+    ui->label->setText(elementySTR);
 }
 
 
@@ -31,5 +40,16 @@ void MainWindow::on_pushButton_2_clicked()
                 elementy[j-1] = elementy[j];
                 elementy[j] = temp;
             };
+QString elementySTR = "";
+QString el;
+for(int i = 0;i<n;i++){
+    el = QString::number(elementy[i]);
+    elementySTR += el + " ";
+}
+QMessageBox msg(this);
+msg.setText(elementySTR);
+msg.setStandardButtons(QMessageBox::Ok);
+msg.setWindowTitle("Sortowanie");
+msg.exec();
 }
 
